5d9714fa64cfced4edf99d1af9c9f16d56ffae70
1KS6 <rrjk.sharma@gmail.com>
2021-04-02 21:23:26 +0530

    basic structure

--- zerodha_brokerage_calculator/lib/screens/homeScreen.dart
+++ zerodha_brokerage_calculator/lib/screens/homeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:relative_scale/relative_scale.dart';
 
@@ -10,7 +11,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController _search = new TextEditingController();
-  final List<int> itemList = List.generate(10, (index) => (index + 1));
+  final List<Widget> itemList = List.generate(
+      3,
+      (index) => Container(
+            color: Colors.white,
+            child: Center(
+              child: Text((index + 1).toString()),
+            ),
+          ));
 
   @override
   void dispose() {
@@ -22,103 +30,23 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return RelativeBuilder(builder: (context, height, width, sy, sx) {
       return Scaffold(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Stack(
-            alignment: Alignment.bottomCenter,
             children: [
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: sy(10)),
-                    Row(children: [
-                      SizedBox(
-                        width: sx(20),
-                      ),
-                      RichText(
-                        textAlign: TextAlign.start,
-                        text: TextSpan(
-                          text: "Hello\n",
-                          children: [
-                            TextSpan(
-                                text: "Foobar",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: sy(18),
-                                    fontWeight: FontWeight.bold)),
-                            TextSpan(
-                                text: "!",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: sy(18),
-                                    fontWeight: FontWeight.w400))
-                          ],
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: sy(16),
-                              fontWeight: FontWeight.w400),
-                        ),
-                      ),
-                    ]),
-                    SizedBox(height: sy(15)),
-                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                      Container(
-                        width: sx(350),
-                        child: TextField(
-                          style: TextStyle(fontSize: sy(13)),
-                          controller: _search,
-                          decoration: InputDecoration(
-                            hintText: "start searching...",
-                            hintStyle: TextStyle(color: Colors.black),
-                            filled: true,
-                            fillColor: Colors.white,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                              BorderRadius.all(Radius.circular(sy(30))),
-                              borderSide:
-                              BorderSide(color: Colors.white, width: 2),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                              BorderRadius.all(Radius.circular(sy(30))),
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            prefixIcon: Padding(
-                              padding: EdgeInsets.only(left: sx(5)),
-                              child: Icon(
-                                Icons.search,
-                                color: Colors.black,
-                                size: sy(23),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    ]),
-                    Column(
-                      children: [
-                        Container(
-                          height: sy(100),
-                          padding: EdgeInsets.only(left: sx(20), top: sy(20)),
-                          child: Container()
-                        )
-                      ],
+              Padding(
+                padding: EdgeInsets.only(top: sy(30)),
+                child: Container(
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      aspectRatio: 2.0,
+                      enlargeCenterPage: true,
+                      scrollDirection: Axis.horizontal,
+                      enableInfiniteScroll: false,
+                      autoPlay: false,
                     ),
-                    SizedBox(height: sy(20)),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Explore from you favorite genres",
-                          style:
-                          TextStyle(color: Colors.white, fontSize: sy(14)),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: sy(10)),
-                    Container()
-                  ],
+                    items: itemList,
+                  ),
                 ),
               ),
             ],
