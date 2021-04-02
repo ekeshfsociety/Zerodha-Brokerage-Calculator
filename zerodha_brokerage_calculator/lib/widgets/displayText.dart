b2f8214e6571d718447b10c5eb4ca8c552c59a15
1KS6 <45872335+1KS6@users.noreply.github.com>
2021-04-03 01:49:49 +0530

    code refactor (#4)

---
+++ zerodha_brokerage_calculator/lib/widgets/displayText.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:relative_scale/relative_scale.dart';
+
+class TextCards extends StatelessWidget {
+  final String name;
+  final double value;
+  TextCards({this.name, this.value});
+  @override
+  Widget build(BuildContext context) {
+    return RelativeBuilder(builder: (context, height, width, sy, sx) {
+      return Padding(
+        padding: EdgeInsets.symmetric(vertical: sy(10)),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: sx(20)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(color: Colors.white, fontSize: sy(12)),
+                  ),
+                  Text(
+                    value.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: sy(12)),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              thickness: 0.5,
+              color: Colors.white,
+            )
+          ],
+        ),
+      );
+    });
+  }
+}
--- zerodha_brokerage_calculator/lib/widgets/equitiesCard.dart
+++ zerodha_brokerage_calculator/lib/widgets/equitiesCard.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:relative_scale/relative_scale.dart';
 import 'package:zerodha_brokerage_calculator/colors.dart';
 
+import 'displayText.dart';
+
 class Equities extends StatefulWidget {
   final Key key;
   final String name;
@@ -226,43 +228,5 @@ class _EquitiesState extends State<Equities> {
                 ],
               ));
         });
-  }
-}
-
-class TextCards extends StatelessWidget {
-  final String name;
-  final double value;
-  TextCards({this.name, this.value});
-  @override
-  Widget build(BuildContext context) {
-    return RelativeBuilder(builder: (context, height, width, sy, sx) {
-      return Padding(
-        padding: EdgeInsets.symmetric(vertical: sy(10)),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: sx(20)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    name,
-                    style: TextStyle(color: Colors.white, fontSize: sy(12)),
-                  ),
-                  Text(
-                    value.toString(),
-                    style: TextStyle(color: Colors.white, fontSize: sy(12)),
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              thickness: 0.5,
-              color: Colors.white,
-            )
-          ],
-        ),
-      );
-    });
   }
 }
