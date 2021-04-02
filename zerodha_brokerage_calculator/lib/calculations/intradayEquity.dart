4de5976853f4a46cb6d3ffbf3e5772b3ed556d4f
ekeshfsociety <51010748+ekeshfsociety@users.noreply.github.com>
2021-04-02 19:22:45 +0530

    Calculations (#1)
    * intradayEquity class added
    
    * intradayEquity class completed

---
+++ zerodha_brokerage_calculator/lib/calculations/intradayEquity.dart
@@ -0,0 +1,50 @@
+class IntraDayEquity {
+  double buy;
+  double sell;
+  int quantity;
+  double turn;
+  double broke;
+  double transac;
+
+  IntraDayEquity(
+    this.buy,
+    this.sell,
+    this.quantity,
+  );
+
+  double turnover() {
+    return ((buy * quantity) + (sell * quantity));
+  }
+
+  double brokerage() {
+    double result = 0;
+    result += (buy * quantity * 0.0003 > 20) ? 20 : (buy * quantity * 0.0003);
+    result += (sell * quantity * 0.0003 > 20) ? 20 : (sell * quantity * 0.0003);
+    return result;
+  }
+
+  double stt() {
+    turn = turnover();
+    return (0.001 * turn);
+  }
+
+  double transactionCharges() {
+    turn = turnover();
+    return (0.0000345 * turn);
+  }
+
+  double gst() {
+    broke = brokerage();
+    transac = transactionCharges();
+    return (0.18 * broke * transac);
+  }
+
+  double sebiCharges() {
+    turn = turnover();
+    return (0.0000005 * turn);
+  }
+
+  double stampCharges() {
+    return (0.00015 * buy * quantity);
+  }
+}
