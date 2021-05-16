4833f51cd006104a6f30beece4331ffe2382e836
Ekesh Taneja <ekesh.taneja1@learner.manipal.edu>
2021-05-17 03:33:15 +0530

    Home screen UI update

--- zerodha_brokerage_calculator/lib/main.dart
+++ zerodha_brokerage_calculator/lib/main.dart
@@ -1,21 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:zerodha_brokerage_calculator/screens/homeScreen.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:zerodha_brokerage_calculator/constants.dart';
+import 'package:zerodha_brokerage_calculator/widgets/category_card.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+String greeting() {
+  var hour = DateTime.now().hour;
+  if (hour > 6 && hour < 12) {
+    return '\nGood Morning \nHappy Trading';
+  }
+  if (hour > 12 && hour < 17) {
+    return '\nGood Afternoon \nHappy Trading';
+  }
+  return '\nGood Evening \nHappy Trading';
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Plaud',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      title: 'Meditation App',
+      theme: ThemeData(
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context)
+        .size; //this gonna give us total height and with of our device
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            // Here the height of the container is 45% of our total height
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    greeting(),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),
+                  SizedBox(
+                    height: 150.0,
+                  ),
+                  Expanded(
+                    child: StaggeredGridView.count(
+                      crossAxisCount: 2,
+                      // childAspectRatio: 0.85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard(
+                          title: "Diet Recommendation",
+                          svgSrc: "assets/icons/Hamburger.svg",
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: "Kegel Exercises",
+                          svgSrc: "assets/icons/Excrecises.svg",
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: "Yoga",
+                          svgSrc: "assets/icons/yoga.svg",
+                          press: () {},
+                        ),
+                      ],
+                      staggeredTiles: [
+                        StaggeredTile.extent(2, 233),
+                        StaggeredTile.extent(1, 233),
+                        StaggeredTile.extent(1, 233)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
