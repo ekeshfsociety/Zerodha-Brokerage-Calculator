c79f0cf8904d15794096f83df3bae08d5df512b5
Ekesh Taneja <ekesh.taneja1@learner.manipal.edu>
2021-05-18 04:15:28 +0530

    Scaling Using relative builder

--- zerodha_brokerage_calculator/lib/main.dart
+++ zerodha_brokerage_calculator/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:relative_scale/relative_scale.dart';
 import 'package:zerodha_brokerage_calculator/constants.dart';
 import 'package:zerodha_brokerage_calculator/widgets/category_card.dart';
 
@@ -40,118 +41,120 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context)
         .size; //this gonna give us total height and with of our device
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            // Here the height of the container is 45% of our total height
-            height: size.height * .45,
-            decoration: BoxDecoration(
-              color: Colors.blue[100],
-              image: DecorationImage(
-                alignment: Alignment.centerLeft,
-                image: AssetImage("assets/images/backgroundhome.png"),
+    return RelativeBuilder(builder: (context, height, width, sy, sx) {
+      return Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              // Here the height of the container is 45% of our total height
+              height: size.height * .45,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  image: AssetImage("assets/images/backgroundhome.png"),
+                ),
               ),
             ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Container(
-                      alignment: Alignment.center,
-                      height: 26,
-                      width: 26,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[100],
-                        shape: BoxShape.circle,
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: sy(26),
+                        width: sy(26),
+                        decoration: BoxDecoration(
+                          color: Colors.blue[100],
+                          shape: BoxShape.circle,
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    greeting(),
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                        fontFamily: 'Cairo'),
-                  ),
-                  Text(
-                    'Happy Trading',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 60,
-                        fontFamily: 'Cairo'),
-                  ),
-                  SizedBox(
-                    height: 60.0,
-                  ),
-                  Expanded(
-                    child: StaggeredGridView.count(
-                      crossAxisCount: 2,
-                      // childAspectRatio: 0.85,
-                      crossAxisSpacing: 20,
-                      mainAxisSpacing: 20,
-                      children: <Widget>[
-                        Neumorphic(
-                          style: NeumorphicStyle(
-                              shape: NeumorphicShape.concave,
-                              boxShape: NeumorphicBoxShape.roundRect(
-                                  BorderRadius.circular(12)),
-                              depth: 8,
-                              lightSource: LightSource.topLeft,
-                              color: Colors.grey),
-                          child: CategoryCard(
-                            title: "Equity",
-                            svgSrc: "assets/icons/equity.svg",
-                            press: () {},
-                          ),
-                        ),
-                        Neumorphic(
-                          style: NeumorphicStyle(
-                              shape: NeumorphicShape.concave,
-                              boxShape: NeumorphicBoxShape.roundRect(
-                                  BorderRadius.circular(12)),
-                              depth: 8,
-                              lightSource: LightSource.topLeft,
-                              color: Colors.grey),
-                          child: CategoryCard(
-                            title: "Commodities",
-                            svgSrc: "assets/icons/commodity.svg",
-                            press: () {},
-                          ),
-                        ),
-                        Neumorphic(
-                          style: NeumorphicStyle(
-                              shape: NeumorphicShape.concave,
-                              boxShape: NeumorphicBoxShape.roundRect(
-                                  BorderRadius.circular(12)),
-                              depth: 8,
-                              lightSource: LightSource.topLeft,
-                              color: Colors.grey),
-                          child: CategoryCard(
-                            title: "Currency",
-                            svgSrc: "assets/icons/exchange.svg",
-                            press: () {},
-                          ),
-                        ),
-                      ],
-                      staggeredTiles: [
-                        StaggeredTile.extent(2, 233),
-                        StaggeredTile.extent(1, 233),
-                        StaggeredTile.extent(1, 233)
-                      ],
+                    Text(
+                      greeting(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: sx(50),
+                          fontFamily: 'Cairo'),
                     ),
-                  ),
-                ],
+                    Text(
+                      'Happy Trading',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: sx(75),
+                          fontFamily: 'Cairo'),
+                    ),
+                    SizedBox(
+                      height: sy(35.0),
+                    ),
+                    Expanded(
+                      child: StaggeredGridView.count(
+                        crossAxisCount: 2,
+                        // childAspectRatio: 0.85,
+                        crossAxisSpacing: 20,
+                        mainAxisSpacing: 20,
+                        children: <Widget>[
+                          Neumorphic(
+                            style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(12)),
+                                depth: 8,
+                                lightSource: LightSource.topLeft,
+                                color: Colors.grey),
+                            child: CategoryCard(
+                              title: "Equity",
+                              svgSrc: "assets/icons/equity.svg",
+                              press: () {},
+                            ),
+                          ),
+                          Neumorphic(
+                            style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(12)),
+                                depth: 8,
+                                lightSource: LightSource.topLeft,
+                                color: Colors.grey),
+                            child: CategoryCard(
+                              title: "Commodities",
+                              svgSrc: "assets/icons/commodity.svg",
+                              press: () {},
+                            ),
+                          ),
+                          Neumorphic(
+                            style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(12)),
+                                depth: 8,
+                                lightSource: LightSource.topLeft,
+                                color: Colors.grey),
+                            child: CategoryCard(
+                              title: "Currency",
+                              svgSrc: "assets/icons/exchange.svg",
+                              press: () {},
+                            ),
+                          ),
+                        ],
+                        staggeredTiles: [
+                          StaggeredTile.extent(2, 233),
+                          StaggeredTile.extent(1, 233),
+                          StaggeredTile.extent(1, 233)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-    );
+            )
+          ],
+        ),
+      );
+    });
   }
 }
