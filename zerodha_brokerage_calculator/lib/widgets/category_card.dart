f6997591881df47bb8a639e4a8526df867a94c6c
Ekesh Taneja <ekesh.taneja1@learner.manipal.edu>
2021-05-23 03:17:36 +0530

    Category card Relative Builder

--- zerodha_brokerage_calculator/lib/widgets/category_card.dart
+++ zerodha_brokerage_calculator/lib/widgets/category_card.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:zerodha_brokerage_calculator/constants.dart';
+import 'package:relative_scale/relative_scale.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class CategoryCard extends StatelessWidget {
   final String svgSrc;
   final String title;
   final Function press;
+
   const CategoryCard({
     Key key,
     this.svgSrc,
@@ -16,51 +18,53 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(13),
-      child: Container(
-        // padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(13),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 17),
-              blurRadius: 17,
-              spreadRadius: -23,
-              color: kShadowColor,
-            ),
-          ],
-        ),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: press,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: <Widget>[
-                  Spacer(),
-                  SvgPicture.asset(
-                    svgSrc,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Spacer(),
-                  Text(
-                    title,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context)
-                        .textTheme
-                        .title
-                        .copyWith(fontSize: 20),
-                  )
-                ],
+    return RelativeBuilder(builder: (context, height, width, sy, sx) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(sy(13)),
+        child: Container(
+          // padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(sy(13)),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 17),
+                blurRadius: 17,
+                spreadRadius: -23,
+                color: kShadowColor,
+              ),
+            ],
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: press,
+              child: Padding(
+                padding: EdgeInsets.all(sy(20.0)),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    SvgPicture.asset(
+                      svgSrc,
+                      width: sx(75),
+                      height: sy(75),
+                    ),
+                    Spacer(),
+                    Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context)
+                          .textTheme
+                          .title
+                          .copyWith(fontSize: sx(20)),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
