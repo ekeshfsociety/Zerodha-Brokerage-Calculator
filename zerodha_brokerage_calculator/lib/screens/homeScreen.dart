5b5ea48712d321242ab46d233f389d8b87f73358
1KS6 <rrjk.sharma@gmail.com>
2021-06-29 21:48:04 +0530

    refactoring

--- zerodha_brokerage_calculator/lib/screens/homeScreen.dart
+++ zerodha_brokerage_calculator/lib/screens/homeScreen.dart
@@ -1,130 +1,149 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:relative_scale/relative_scale.dart';
-import 'package:zerodha_brokerage_calculator/widgets/currecncyCard.dart';
-import 'package:zerodha_brokerage_calculator/widgets/equitiesCard.dart';
-import '../widgets/titles.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:zerodha_brokerage_calculator/screens/commoditiesScreen.dart';
+import 'package:zerodha_brokerage_calculator/screens/currencyScreen.dart';
+import 'package:zerodha_brokerage_calculator/screens/equityScreen.dart';
+import 'package:zerodha_brokerage_calculator/widgets/category_card.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
 
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  TextEditingController _search = new TextEditingController();
-  int _currentIndex = 0;
-  int pageNum = 0;
-  final List<Widget> itemList = List.generate(
-      3,
-      (index) => Container(
-            color: Color(0xff001D3D),
-            child: Center(
-              child: Text((index + 1).toString()),
-            ),
-          ));
-
-  List<Widget> item = [
-    Equities(key: Key("1"), name: "Intraday Equity"),
-    Equities(key: Key("2"), name: "Delivery Equity"),
-    Equities(key: Key("3"), name: "F&O - Futures"),
-    Equities(key: Key("4"), name: "F&O - Options")
-  ];
-  List<Widget> itemCurrency = [
-    Currency(key: Key("1"), name: "Futures Currency", isFutures: true),
-    Currency(key: Key("2"), name: "Options Currency", isFutures: false)
-  ];
-  List listDisplayed;
-
-  @override
-  void initState() {
-    super.initState();
-    listDisplayed = [item,itemCurrency,item];
+  String greeting() {
+    var hour = DateTime.now().hour;
+    if (hour > 6 && hour < 12) {
+      return '\nGood Morning';
+    }
+    if (hour > 12 && hour < 17) {
+      return '\nGood Afternoon';
+    }
+    return '\nGood Evening';
   }
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context)
+        .size; //this gonna give us total height and with of our device
     return RelativeBuilder(builder: (context, height, width, sy, sx) {
       return Scaffold(
-        backgroundColor: Color(0xff000814),
-        body: SafeArea(
-          child: Stack(
-            children: [
-              SingleChildScrollView(
-                physics: BouncingScrollPhysics(),
-                child: Padding(
-                  padding: EdgeInsets.only(top: sy(60)),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: sy(50),
-                        width: double.infinity,
-                        child: CarouselSlider(
-                          options: CarouselOptions(
-                              aspectRatio: 2.0,
-                              enlargeCenterPage: true,
-                              viewportFraction: 0.8,
-                              scrollDirection: Axis.horizontal,
-                              enableInfiniteScroll: false,
-                              disableCenter: false,
-                              autoPlay: false,
-                              onPageChanged:
-                                  (index, carouselPageChangedReason) {
-                                setState(() {
-                                  _currentIndex = index;
-                                  if(_currentIndex == 1 && pageNum == 2){
-                                    pageNum = 1;
-                                  }
-                                });
-                              }),
-                          items: [
-                            TitleText(
-                              name: "Equities - F&O",
-                              selected: _currentIndex == 0,
-                            ),
-                            TitleText(
-                              name: "Currency",
-                              selected: _currentIndex == 1,
-                            ),
-                            TitleText(
-                              name: "Commodities",
-                              selected: _currentIndex == 2,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: sy(10)),
-                        child: Container(
-                          child: CarouselSlider(
-                              options: CarouselOptions(
-                                height: sy(610),
-                                enlargeCenterPage: true,
-                                viewportFraction: 0.8,
-                                initialPage: pageNum,
-                                scrollDirection: Axis.horizontal,
-                                enableInfiniteScroll: false,
-                                disableCenter: true,
-                                autoPlay: false,
-                                onPageChanged: (num, carouselPageChangedReason){
-                                  setState(() {
-                                    pageNum = num;
-                                  });
-                                }
-                              ),
-                              items: listDisplayed[_currentIndex]),
-                        ),
-                      ),
-                      SizedBox(
-                        height: sy(10),
-                      )
-                    ],
-                  ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              // Here the height of the container is 45% of our total height
+              height: size.height * .45,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  image: AssetImage("assets/images/backgroundhome.png"),
                 ),
               ),
-            ],
-          ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: sx(20)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: sy(20)),
+                    Text(
+                      greeting(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: sx(50),
+                          fontFamily: 'Cairo'),
+                    ),
+                    SizedBox(height: sy(15)),
+                    Text(
+                      'Happy Trading',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: sx(75),
+                          fontFamily: 'Cairo'),
+                    ),
+                    SizedBox(
+                      height: sy(40.0),
+                    ),
+                    Expanded(
+                      child: StaggeredGridView.count(
+                        crossAxisCount: 2,
+                        // childAspectRatio: 0.85,
+                        crossAxisSpacing: sx(20),
+                        mainAxisSpacing: sy(20),
+                        children: <Widget>[
+                          Neumorphic(
+                            style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(sy(12))),
+                                depth: sy(8),
+                                lightSource: LightSource.topLeft,
+                                color: Colors.grey),
+                            child: CategoryCard(
+                              title: "Equity",
+                              svgSrc: "assets/icons/equity.svg",
+                              press: () {
+                                Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (context) =>
+                                        new EquityScreen()));
+                              },
+                            ),
+                          ),
+                          Neumorphic(
+                            style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(sy(12))),
+                                depth: sy(8),
+                                lightSource: LightSource.topLeft,
+                                color: Colors.grey),
+                            child: CategoryCard(
+                              title: "Currency",
+                              svgSrc: "assets/icons/exchange.svg",
+                              press: () {
+                                Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (context) =>
+                                        new CurrencyScreen()));
+                              },
+                            ),
+                          ),
+                          Neumorphic(
+                            style: NeumorphicStyle(
+                                shape: NeumorphicShape.concave,
+                                boxShape: NeumorphicBoxShape.roundRect(
+                                    BorderRadius.circular(sy(12))),
+                                depth: sy(8),
+                                lightSource: LightSource.topLeft,
+                                color: Colors.grey),
+                            child: CategoryCard(
+                              title: "Commodities",
+                              svgSrc: "assets/icons/commodity.svg",
+                              press: () {
+                                Navigator.push(
+                                    context,
+                                    new MaterialPageRoute(
+                                        builder: (context) =>
+                                        new CommoditiesScreen()));
+                              },
+                            ),
+                          ),
+                        ],
+                        staggeredTiles: [
+                          StaggeredTile.extent(2, sy(145)),
+                          StaggeredTile.extent(1, sy(145)),
+                          StaggeredTile.extent(1, sy(145))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       );
     });
